7a11bcb5496021ce0bf6fffaddab293b192a2a07
angelocaamano-hub <angelo.caamano@escola.pr.gov.br>
2025-10-23 09:30:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addElementeLitener('click', function(){
+        const atual = document.querrySelector('.ativo')
+        const proximoPasso = 'passo' + this.getAtribute('data-proximo')
+
+        atual.classList.remove('ativo')
+        document.getElementById(proximoPasso).classList.add('ativo')
+    })
+    
+});
+    
